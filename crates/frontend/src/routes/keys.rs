649161153a5eb0b5afdb545423ae8fe6da5ea795
Mike Lloyd <mike.lloyd03@pm.me>
2022-08-12 01:00:29 -0700

    Keys table fixed

--- crates/frontend/src/routes/keys.rs
+++ crates/frontend/src/routes/keys.rs
@@ -57,19 +57,32 @@ pub fn keys() -> Html {
         Key {
             name: "key4".to_string(),
             description: "this is key for".to_string(),
-            active: true,
+            active: false,
         },
     ];
+
     html! {
         <div class="container text-light my-3">
             <div class="row justify-content-center">
                 <Table title="Keys">
-                    <Row>
-                        <Cell heading="Key" value="key1" />
-                        <Cell heading="Description" value="This is key 1" />
-                        <Cell heading="Status" value="Active" />
-                        <Cell heading="" value="Edit" />
-                    </Row>
+                    { for keys
+                        .iter()
+                        .map(|key| {
+                            html_nested! {
+                                <Row>
+                                    <Cell heading="Key" value={ key.name.clone() } />
+                                    <Cell heading="Description" value={ key.description.clone() } />
+                                    <Cell heading="Status" value={
+                                        match key.active {
+                                            true => "Active",
+                                            false => "Inactive",
+                                        }
+                                    } />
+                                    <Cell heading="" value="Edit" />
+                                </Row>
+                            }
+                        })
+                    }
                 </Table>
             </div>
         </div>
--- crates/frontend/src/routes/table.rs
+++ crates/frontend/src/routes/table.rs
@@ -1,11 +1,5 @@
 use yew::prelude::*;
 
-// Tables headers can be derived from the table rows
-// We need three types:
-// - Table
-// - Row
-// - Cell
-
 #[function_component(Cell)]
 pub fn cell(props: &CellProps) -> Html {
     html! {
@@ -31,35 +25,76 @@ pub struct RowProps {
     pub children: ChildrenWithProps<Cell>,
 }
 
-#[function_component(Table)]
-pub fn table(props: &TableProps) -> Html {
-    let headings: Vec<String> = props
-        .children
-        .iter()
-        .next()
-        .unwrap()
-        .props
-        .children
-        .iter()
-        .map(|cell| cell.props.heading.clone())
-        .collect();
+pub struct Table;
 
-    html! {
-        <div style="text-align: center">
-            <h2>{ props.title.clone() }</h2>
-                <table class="table table-striped table-hover table-bordered table-dark">
-                    <thead class="table-dark">
-                        <tr>
-                            { for headings.iter().map(|heading| html!{<th>{ heading.clone() }</th>}) }
-                        </tr>
-                    </thead>
-                    <tbody>
-                        { for props.children.iter()}
-                    </tbody>
-                </table>
-        </div>
+impl Component for Table {
+    type Message = ();
+    type Properties = TableProps;
+
+    fn create(_: &Context<Self>) -> Self {
+        Self
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        let headings: Vec<String> = ctx
+            .props()
+            .children
+            .iter()
+            .next()
+            .unwrap()
+            .props
+            .children
+            .iter()
+            .map(|cell| cell.props.heading.clone())
+            .collect();
+
+        html! {
+            <div style="text-align: center">
+                <h2>{ ctx.props().title.clone() }</h2>
+                    <table class="table table-striped table-hover table-bordered table-dark">
+                        <thead class="table-dark">
+                            <tr>
+                                { for headings.iter().map(|heading| html!{<th>{ heading.clone() }</th>}) }
+                            </tr>
+                        </thead>
+                        <tbody>
+                            { for ctx.props().children.iter()}
+                        </tbody>
+                    </table>
+            </div>
+        }
     }
 }
+
+// #[function_component(Table)]
+// pub fn table(props: &TableProps) -> Html {
+//     let headings: Vec<String> = props
+//         .children
+//         .iter()
+//         .next()
+//         .unwrap()
+//         .props
+//         .children
+//         .iter()
+//         .map(|cell| cell.props.heading.clone())
+//         .collect();
+
+//     html! {
+//         <div style="text-align: center">
+//             <h2>{ props.title.clone() }</h2>
+//                 <table class="table table-striped table-hover table-bordered table-dark">
+//                     <thead class="table-dark">
+//                         <tr>
+//                             { for headings.iter().map(|heading| html!{<th>{ heading.clone() }</th>}) }
+//                         </tr>
+//                     </thead>
+//                     <tbody>
+//                         { for props.children.iter()}
+//                     </tbody>
+//                 </table>
+//         </div>
+//     }
+// }
 
 #[derive(Properties, PartialEq)]
 pub struct TableProps {
